41b05263fa5dc462a1c1117027bd7addec14e0c5
Ikram Kharbouch <ikrkharb@e1r9p8.1337.ma>
2019-05-25 23:59:16 +0000

    commit changes

--- place_tetros.c
+++ place_tetros.c
@@ -6,7 +6,7 @@
 /*   By: ikrkharb <ikrkharb@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 19:10:21 by ikrkharb          #+#    #+#             */
-/*   Updated: 2019/05/24 23:58:36 by ikrkharb         ###   ########.fr       */
+/*   Updated: 2019/05/25 23:50:35 by ikrkharb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,44 @@
 
 void	place_tetros(t_node *head)
 {
-	char	***temp;
-	int		length;
-	int		i;
+	int i;
+	int j;
+	int min_i;
+	int min_j;
+	int	max_i;
+	int max_j;
 
-	length = list_size(head);
-	if (!(temp = (char ***)malloc(sizeof(char *) * (length + 1))))
-		exit(1);
-	i = 0;
 	while (head)
 	{
-		temp[i] = head->content;
-		ft_print_array(temp[i], 0);
+		min_i = 5;
+		min_j = 5;
+		max_i = -1;
+		max_j = -1;
+		i = 0;
+		while (i < 4)
+		{
+			j = 0;
+			while (j < 4)
+			{
+				if (head->content[i][j] == '#')
+				{
+					min_j = min_j > j ? j : min_j;
+					max_j = max_j < j ? j : max_j;
+					min_i = min_i > i ? i : min_i;
+					max_i = max_i < i ? i : max_i;
+				}
+				j++;
+			}
+			i++;
+		}
+		i = min_i - 1;
+		while (++i <= max_i)
+		{
+			j = min_j - 1;
+			while (++j <= max_j)
+				ft_putchar(head->content[i][j]);
+			ft_putchar('\n');
+		}
 		head = head->next;
-		i++;
 	}
-	ft_del_array(*temp);
-	ft_memdel((void **)&temp);
-}
-	
+}	
